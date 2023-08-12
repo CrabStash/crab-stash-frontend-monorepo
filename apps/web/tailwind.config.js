27f181cda40a336311d40c140053bbd08759c8a5
hoolek77 <szymon.kin@gmail.com>
2023-08-12 11:58:49 +0200

    fix(CRAB-29): Including components from ui package

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -4,6 +4,6 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "../../packages/ui/components/*",
+    "../../packages/ui/components/**/*.{js,ts,jsx,tsx}",
   ],
 };
