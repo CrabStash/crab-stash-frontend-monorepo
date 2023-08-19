f8edc962fcb016dccdc5baf256439b2e77f180fb
hoolek77 <szymon.kin@gmail.com>
2023-08-19 15:19:59 +0200

    chore(CRAB-29): Migrate to native next.js transpilePackages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,3 @@
-const withTM = require("next-transpile-modules")(["@crab-stash/ui"]);
-
-module.exports = withTM({
-  reactStrictMode: true,
-});
+module.exports = {
+  transpilePackages: ["@crab-stash/ui"],
+};
